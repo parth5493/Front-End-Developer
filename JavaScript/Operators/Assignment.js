fe8b1482f0e6c5fb0b0f918f9b68750fadfd4ac8
Parthkumar Patel <126305330+parth5493@users.noreply.github.com>
2023-04-24 18:54:25 -0400

    Add files via upload

---
+++ JavaScript/Operators/Assignment.js
@@ -0,0 +1,9 @@
+let x = 5
+
+//console.log(x += 6);
+
+//console.log(x -= 6);
+
+//console.log(x *= 6);
+
+//console.log(x /= 6);
---
+++ JavaScript/Operators/Comparision.js
@@ -0,0 +1,10 @@
+let a = 5
+let b = 6
+let c = "5"
+
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a === c);
+console.log(a !== c);
